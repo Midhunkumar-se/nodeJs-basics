b5cae84f83eea292f8a083aff74c47d99cd953b2
Midhunkumar-se <midhu025@gmail.com>
2026-08-04 21:52:47 +0530

    promise asyn await 2

--- src/06-promise-async-await-module.ts
+++ src/06-promise-async-await-module.ts
@@ -1,3 +1,5 @@
+import { error } from "node:console";
+
 type User = {
   id: number;
   name: string;
@@ -28,4 +30,59 @@ const users: User[] = [
 function findUserWithCallback(
   userId: number,
   callback: (error: Error | null, user?: User) => void,
-) {}
+): void {
+  setTimeout(() => {
+    // ur actual api call
+    const user = users.find((currentUser) => currentUser.id === userId);
+    if (!user) {
+      callback(new Error(`user with id ${userId} was not found`));
+      return;
+    }
+
+    callback(null, user);
+  }, 500);
+}
+
+// findUserWithCallback(30, (error, user) => {
+//   if (error) {
+//     console.log("callback error", error.message);
+//     return;
+//   }
+
+//   console.log("callback result", user?.id, user?.name, user?.role);
+// });
+
+function fetchUserWithPromise(userId: number): Promise<User> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const user = users.find((currentUser) => currentUser.id === userId);
+
+      if (!user) {
+        reject(new Error(`user with ${userId} data was not found`));
+        return;
+      }
+
+      resolve(user);
+    }, 1000);
+  });
+}
+
+// fetchUserWithPromise(20)
+//   .then((user) => {
+//     console.log("Promise result", user?.id, user?.name, user?.role);
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//   });
+
+function findUserWithPromise(userId: number): Promise<User> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {}, 1000);
+  });
+}
+
+async function findUserWithAsyncAwait(userId: number): Promise<void> {
+  try {
+    const user = await findUserWithPromise(userId);
+  } catch (error) {}
+}
